cebc04de8409049fd961f63cf91d572939e1a673
Sami Fares <djsamif@gmail.com>
2022-03-11 15:26:57 -0500

    inquirer prompts

--- index.js
+++ index.js
@@ -1,9 +1,74 @@
 // TODO: Required packages inquirer, fs, md function from generate markdown file
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generatedMD = require(`./utils/generateMarkdown.js`);
 
 // TODO: write an inqurier prompt to match acceptance criteria
 
+inquirer
+    .prompt([
+        {
+            type: `input`,
+            message: `What is the name of your project?`,
+            name: `projectName`,
+        },
+        {
+            type: `input`,
+            message: `Please type a quick description of your project`,
+            name: `description`,
+        },
+        {
+            type: `input`,
+            message: `Was there an installation process/instructions?`,
+            name: `installation`,
+        },
+        {
+            type: `input`,
+            message: `What could this project be used for?`,
+            name: `usage`,
+        },
+        {
+            type: `list`,
+            message: `Which license would you like to apply to this project?`,
+            name: `license`,
+            choices: [
+                "MIT",
+                "Mozzila",
+                "Open"
+            ]
 
-// in .then of inquirer prompt call fs write file
+        },
+        {
+            type: `input`,
+            message: `Who contributed to this project?`,
+            name: `contribution`,
+        },
+        {
+            type: `input`,
+            message: `What are the test instructions?`,
+            name: `test`,
+        },
+        {
+            type: `input`,
+            message: `What is your github username?`,
+            name: `github`,
+        },
+        {
+            type: `input`,
+            message: `What is your email address?`,
+            name: `email`,
+        }
+
+
+
+    ])
+        .then((data) => {
+            const 
+        }
+
+
+       
+        // in .then of inquirer prompt call fs write file
 
 
 // in fs write file (2) arguments first argument 1. file path file name 2. data (generateMarkdown)
